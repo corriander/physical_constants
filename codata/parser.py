89d1e3715ff1f03d956b5be62ef2630062732e52
Alex Corrie <ajccode@gmail.com>
2014-02-21 15:13:04 +0000

    Altered the basic record parsing implementation.
    Had a bit of a rethink, the table records are better represented a bit
    more meaningfully as specialised objects. I still see value in
    breaking this parsing process down into a multi-stage one for
    testing/validation purposes so this just makes the code a bit tidier.

--- codata/parser.py
+++ codata/parser.py
@@ -1,5 +1,10 @@
 import re
 
+CODATARecord = namedtuple(
+	'CODATARecord',
+	'name, value, uncertainty, units'
+	)
+
 class Parser(object):
 	"""Parse NIST CODATA ASCII table"""
 
@@ -14,11 +19,15 @@ class Parser(object):
 				if line.startswith('-----'):
 					in_table = True
 			return dat.readlines()
-	
-	def _parse_columns(self):
-		# Split table into columns via regex
+
+	@property
+	def records(self):
+		"""Table records as a list of CODATARecord named tuples.
+		
+		Simply splits each row by contiguous whitespace into fields
+		and stores them as an immutable object. The records are
+		composed of the original, unmodified strings.
+		
+		"""
 		regex = re.compile(r'\s{2,}')    # Contiguous whitespace
-		l = []
-		for row in self.read():
-			l.append(tuple(regex.split(row)))
-		return l    # List of tuples
+		return map(CODATARecord._make, map(regex.split, self.read()))
