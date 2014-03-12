2c0a688e377184e76c0fb91dc50f693a83e1e05c
Alex Corrie <ajccode@gmail.com>
2014-03-12 15:03:56 +0000

    Basic tests for data integrity in parser.records

--- codata/tests/test_parser.py
+++ codata/tests/test_parser.py
@@ -1,4 +1,5 @@
 import unittest
+import re
 from codata.parser import Parser
 
 class TestParser(unittest.TestCase):
@@ -9,41 +10,45 @@ class TestParser(unittest.TestCase):
 				version : Parser(version)
 				for version in versions
 				}
-		regex_units = { 'start' : re.compile('[a-zA-Z()]') }
-		regex_uncer = { 'start' : re.compile('0\.\d|\(exact\)') }
-		regex_num = { 'full' : re.compile(
-							'[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?'
-							) }
-		self.regex = {
-				'units' : regex_units,
-				'uncertainty' : regex_uncer,
-				'value' : regex_num
-				}
+		self.re_unit = re.compile(
+				 r"""
+				 ( 						# BEGIN optional group
+				 (ohm|GeV|mol|MeV|E_h|  # List of possible units
+				 Hz|Wb|MHz|eV|A|C|C_90|
+				 F|K|J|N|S|T|W|V|fm|c|
+				 kg|sr|m|s|Pa|u|
+				 \(GeV/c\^2\)|MeV/c)    #   Special cases
+				 (\^-?\d+)?				# exponent (optional)
+				 \s? 					# space (optional)
+				 )*                     # END optional group
+				 $						# End of string
+				 """, re.VERBOSE)
+		self.re_num = re.compile(
+				r"""
+				(-?(\d+[.\s]?)+(\.{3})?\s?(e-?\d+)?)
+				|
+				(\(exact\))
+				""", re.VERBOSE)
 	
-	def test_parser_read(self):
+	def test_parser_reads_all_records(self):
 		# Dumb check to ensure parser reads from each version
 		# properly.
 		nrecords = {2002 : 309, 2006 : 326, 2010 : 335}
 		for version, parser in self.parsers.items():
 			returned_list = parser.read()
-			self.assertIsInstance(returned_list, list)
 			self.assertTrue(len(returned_list), nrecords[version]) 
 	
-	def test_parser_records(self):
+	def test_parser_records_match_regex(self):
 		# Make sure the parser parses each table with no obvious 
 		# errors (i.e. it splits each row into 4) and cols 2-4 match
 		# the regex
-		re_unit = re.compile(
-				r'((ohm|GeV|mol|MeV|E_h|Hz|Wb|MHz|eV|A|C|'
-		         'C_90|F|K|J|N|S|T|W|V|fm|c|kg|sr|m|s|Pa|u)'
-				 '( |^-?\d+)*)*$'
-				 )
-		re_num = re.compile(r'-?(\d+[. ]?)+(\.{3})? ?(e-?\d+)?')
 		for version, parser in self.parsers.items():
-			returned_list = parser.parse()
-			self.assertIsInstance(returned_dict, list)
+			returned_list = parser.records
 			for record in returned_list:
 				self.assertTrue(len(record) == 4)
-				self.assertTrue(re_unit.match(record.units))
-				self.assertTrue(re_num.match(record.value))
-				self.assertTrue(re_num.match(record.uncertainty))
+				self.assertTrue(self.re_unit.match(record.units))
+				self.assertTrue(self.re_num.match(record.value))
+				self.assertTrue(self.re_num.match(record.uncertainty))
+
+if __name__ == '__main__':
+	unittest.main()
